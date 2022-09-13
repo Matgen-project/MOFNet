e3dab175a5af4829b6417c312ddf3c373cb1ca37
Pin Chen <chenxupin@yeah.net>
2022-09-13 11:25:30 +0800

    Create create_geo_features.sh

---
+++ process/create_geo_features.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+data_dir=${root_dir_for_cifs}/
+cell_num=$2
+i=$1
+name=`echo $i |cut -d '.' -f 1`
+#argument 1 and 2
+#../network -ha -res ~/wsl/work/clean/$i >/dev/null
+~/bin/network -ha -res ${data_dir}/${i} >/dev/null
+LCD=`head -n 1  ${data_dir}/${name}.res | awk '{print $4}'`
+PLD=`head -n 1  ${data_dir}/${name}.res | awk '{print $3}'`
+#exit
+rm  ${data_dir}/${name}.res
+#argument 3 and 4 and 5
+#../network -ha -sa 1.86 1.86 2000 ~/wsl/work/clean/$i >/dev/null
+~/bin/network -ha -sa 1.86 1.86 2000 ${data_dir}/${i} >/dev/null
+VSA=`head -n 1  ${data_dir}/${name}.sa | awk '{print $10}'`
+GSA=`head -n 1  ${data_dir}/${name}.sa | awk '{print $12}'`
+Density=`head -n 1  ${data_dir}/${name}.sa | awk '{print $6}'`
+chan_num_sa=`sed -n '2p'  ${data_dir}/${name}.sa | awk '{print $2}'`
+rm  ${data_dir}/${name}.sa
+#argument 6 and 7  
+#   ../network -ha -vol 0 0 50000 ~/wsl/work/clean/$i >/dev/null
+~/bin/network -ha -vol 0 0 50000 ${data_dir}/${i} >/dev/null
+voidfract=`head -n 1  ${data_dir}/${name}.vol | awk '{print $10}'` porevolume=`head -n 1  ${data_dir}/${name}.vol | awk '{print $12}'`
+rm  ${data_dir}/${name}.vol
+CN=`echo "scale=6;${CN}/${cell_num}" | bc`
+PV=`echo "scale=6;${PV}/${cell_num}" | bc`
+OMS=`echo "scale=6;${OMS}/${cell_num}" | bc`
+printf "%-20s%-10s%-10s%-10s%-10s%-10s%-10s%-15s%-15s\n" $name $Density $PLD $LCD $VSA $GSA $voidfract $porevolume $chan_num_sa
